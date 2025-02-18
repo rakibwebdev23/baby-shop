f4bda4b9d1796fd73ebdd8cf339d1a5b4759e9aa
Rakib <rh.rakibhasan365@gmail.com>
2025-02-18 12:40:26 +0600

    environment set

--- src/authentication/firebase/firebase.config.js
+++ src/authentication/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-QOUC1D6h38e3GemqBNBUbqlhHryNNag",
-  authDomain: "baby-shop-92a34.firebaseapp.com",
-  projectId: "baby-shop-92a34",
-  storageBucket: "baby-shop-92a34.firebasestorage.app",
-  messagingSenderId: "994330838194",
-  appId: "1:994330838194:web:95ac53c91a1d9ffe8d75ab"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
--- src/components/ProductCart/ProductCart.jsx
+++ src/components/ProductCart/ProductCart.jsx
@@ -1,11 +1,68 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useProducts from "../../hooks/useProducts";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const ProductCart = ({ item }) => {
-    const { image, name, price, category, productNumber } = item;
+    const { image, name, price, category, productNumber, _id } = item;
+    const [, refetch] = useProducts();
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleAddOrder = () => {
+        if (user && user?.email) {
+            const cartData = {
+                cartId: _id,
+                productNumber,
+                email: user.email,
+                category,
+                name,
+                image,
+                price
+            }
+
+            axiosSecure.post("/carts", cartData)
+                .then(res => {
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            position: "top-center",
+                            icon: "success",
+                            title: `${name} added to your cart`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        refetch();
+                    }
+
+                })
+        }
+        else {
+            Swal.fire({
+                title: "You are not Sign In",
+                text: "Please Sign In & add to the cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Please! Sign In"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate("/login", { state: { from: location } })
+                }
+            });
+        }
+    }
+
     return (
         <div className="group relative border border-gray-200 bg-white hover:shadow-md p-2">
             <div className="aspect-w-1 aspect-h-1 overflow-hidden">
                 <img className="h-48 w-full object-cover rounded-t-md" src={image} alt={name} loading="lazy" />
                 <div className="absolute inset-0 bg-pink-300/40 opacity-0 group-hover:opacity-100">
-                    <button className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#FF8080] hover:bg-[#f45e5e] px-4 py-2 text-sm font-medium text-white rounded">                         Add to Cart
+                    <button onClick={handleAddOrder} className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#FF8080] hover:bg-[#f45e5e] px-4 py-2 text-sm font-medium text-white rounded">
+                        Add to Cart
                     </button>
                 </div>
             </div>
